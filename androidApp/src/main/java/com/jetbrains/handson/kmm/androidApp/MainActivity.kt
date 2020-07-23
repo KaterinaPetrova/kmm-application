1f9e2897213b753271b6b2c9c6499b1e6462af04
Ekaterina Petrova <91kathrin@gmail.com>
2020-07-23 15:09:34 +0300

    Store and display test data

--- androidApp/src/main/java/com/jetbrains/handson/kmm/androidApp/MainActivity.kt
+++ androidApp/src/main/java/com/jetbrains/handson/kmm/androidApp/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.jetbrains.handson.kmm.androidApp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.jetbrains.handson.kmm.sdk.SpaceXSDK
+import com.jetbrains.handson.kmm.sdk.database.DatabaseDriverFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tv: TextView = findViewById(R.id.text_view)
+        val sdk = SpaceXSDK(DatabaseDriverFactory(applicationContext))
+        tv.text = sdk.getFirstLaunchName()
     }
 }
 
--- sdk/src/commonMain/kotlin/SpaceXSDK.kt
+++ sdk/src/commonMain/kotlin/SpaceXSDK.kt
@@ -2,8 +2,57 @@ package com.jetbrains.handson.kmm.sdk
 
 import com.jetbrains.handson.kmm.sdk.database.DatabaseDriverFactory
 import com.jetbrains.handson.kmm.sdk.database.AppDatabase
+import com.jetbrains.handson.kmm.sdk.entity.*
 
 class SpaceXSDK(databaseDriverFactory: DatabaseDriverFactory) {
     private val db = AppDatabase(databaseDriverFactory.createDriver())
+
+    init {
+        db.appDatabaseQueries.removeAllLaunches()
+        db.appDatabaseQueries.insertLaunch(flightNumber = 1,
+            missionName = "test1",
+            launchYear = 1991,
+            rocketId = "1",
+            details = "",
+            launchSuccess = true,
+            launchDateUTC = "",
+            missionPatchUrl = null,
+            articleUrl = ""
+        )
+    }
+
+    fun getFirstLaunchName(): String {
+        val list: List<RocketLaunch> = db.appDatabaseQueries.selectAllLaunchesInfo(::mapLaunchSelecting).executeAsList()
+        return list.first().missionName
+    }
+
+    private fun mapLaunchSelecting(
+        flightNumber: Long,
+        missionName: String,
+        launchYear: Int,
+        rocketId: String,
+        details: String?,
+        launchSuccess: Boolean?,
+        launchDateUTC: String,
+        missionPatchUrl: String?,
+        articleUrl: String?,
+        rocket_id: String?,
+        name: String?,
+        type: String?
+    ): RocketLaunch {
+        return RocketLaunch(
+            flightNumber = flightNumber.toInt(),
+            missionName = missionName,
+            launchYear = launchYear,
+            details = details,
+            launchDateUTC = launchDateUTC,
+            launchSuccess = launchSuccess,
+            rocket = null,
+            links = Links(
+                missionPatchUrl = missionPatchUrl,
+                articleUrl = articleUrl
+            )
+        )
+    }
 
 }
