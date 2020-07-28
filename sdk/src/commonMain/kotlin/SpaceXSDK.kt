7f67ee99f6010485afb96269a16ccc09b98e2c54
Ekaterina Petrova <91kathrin@gmail.com>
2020-07-28 22:36:10 +0300

    step-07: build SDK

--- sdk/src/commonMain/kotlin/SpaceXSDK.kt
+++ sdk/src/commonMain/kotlin/SpaceXSDK.kt
@@ -1,10 +1,22 @@
 package com.jetbrains.handson.kmm.sdk
 import com.jetbrains.handson.kmm.sdk.cache.Database
 import com.jetbrains.handson.kmm.sdk.cache.DatabaseDriverFactory
+import com.jetbrains.handson.kmm.sdk.entity.RocketLaunch
 import com.jetbrains.handson.kmm.sdk.network.SpaceXApi
 
 class SpaceXSDK(databaseDriverFactory: DatabaseDriverFactory) {
-    private val database by lazy { Database(databaseDriverFactory) }
-    private val api by lazy { SpaceXApi() }
+    private val database = Database(databaseDriverFactory)
+    private val api = SpaceXApi()
 
+    suspend fun getLaunches(forceReload: Boolean): List<RocketLaunch> {
+        val cachedLaunches = database.getAllLaunches()
+        return if (cachedLaunches.isNotEmpty() && !forceReload) {
+            cachedLaunches
+        } else {
+            api.getAllLaunches().also {
+                database.clearDatabase()
+                database.createLaunches(it)
+            }
+        }
+    }
 }
